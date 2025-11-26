142814758e672660e573042eeb970dea349b2a4b
Eren Çit <erean.55.44@outlook.com>
2025-11-26 23:25:55 +0300

    feat: authMiddleware added

---
+++ backend/src/middlewares/auth.middleware.ts
@@ -0,0 +1,51 @@
+import { NextFunction, Request, Response } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { ACCESS_SECRET, REFRESH_SECRET } from "../config/env.config";
+import { CustomError } from "../lib/customError";
+import { AuthRepository } from "../repositories/auth.repository";
+
+const authRepository = new AuthRepository();
+export const authMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const accessToken = req.cookies.accessToken || "";
+  const refreshToken = req.cookies.refreshToken || "";
+  try {
+    if (accessToken && accessToken !== "") {
+      const decodedData = jwt.verify(accessToken, ACCESS_SECRET!) as JwtPayload;
+      req.user = decodedData;
+      next();
+    }
+    if (!refreshToken && refreshToken === "")
+      throw new CustomError("RefreshToken missing");
+    const decodeRefresh = jwt.verify(
+      refreshToken,
+      REFRESH_SECRET!
+    ) as JwtPayload;
+
+    const userDatas = await authRepository.findById(Number(decodeRefresh.id));
+
+    const newUserData = {
+      id: userDatas.id,
+      username: userDatas.username,
+      email: userDatas.email,
+      createdAt: userDatas.created_at,
+    };
+
+    const newAccessToken = jwt.sign(newUserData, ACCESS_SECRET!, {
+      expiresIn: "1h",
+    });
+
+    res.cookie("accesToken", newAccessToken, {
+      httpOnly: true,
+      sameSite: "strict",
+      maxAge: 1000 * 60 * 60,
+    });
+
+    next();
+  } catch (error: any) {
+    throw new CustomError(error.message);
+  }
+};
--- backend/src/repositories/auth.repository.ts
+++ backend/src/repositories/auth.repository.ts
@@ -1,7 +1,10 @@
 import { prisma } from "../lib/prisma";
 import { User } from "@prisma/client";
 import { RegisterUserDto, resetPasswordDto } from "../dto/auth.dto";
-import { expireDateHandlerBasedMinutes, hashPassword } from "../helpers/auth.helpers";
+import {
+  expireDateHandlerBasedMinutes,
+  hashPassword,
+} from "../helpers/auth.helpers";
 import { CustomError } from "../lib/customError";
 export class AuthRepository {
   public async findByEmail(email: string): Promise<User | null> {
@@ -29,70 +32,94 @@ export class AuthRepository {
       throw new CustomError(error.message);
     }
   }
-  public async createUser(body: RegisterUserDto,verifyOtp : string): Promise<User> {
+  public async findById(userId: number): Promise<User> {
+    try {
+      const user: User | null = await prisma.user.findUnique({
+        where: {
+          id: userId,
+        },
+      });
+      if (!user) throw new CustomError("User not found",404)
+      return user;
+    } catch (error: any) {
+      throw new CustomError(error.message);
+    }
+  }
+  public async createUser(
+    body: RegisterUserDto,
+    verifyOtp: string
+  ): Promise<User> {
     let { username, password, email } = body;
     const hashedPw = await hashPassword(password);
     try {
-      const veriyOtpExpireAt = expireDateHandlerBasedMinutes(30)
+      const veriyOtpExpireAt = expireDateHandlerBasedMinutes(30);
       const user = await prisma.user.create({
         data: {
           username,
           password: hashedPw,
           email,
-          verify_otp : verifyOtp,
-          verify_otp_expire_at : veriyOtpExpireAt
+          verify_otp: verifyOtp,
+          verify_otp_expire_at: veriyOtpExpireAt,
         },
       });
 
       return user;
     } catch (error: any) {
       throw new CustomError(error.stack);
-    }          
-  }
-  public async updateUserVerified (user : User) : Promise<void> {
-    const {email,is_verified,verify_otp} = user
-    try{ 
-      await prisma.user.update({
-        where : {
-          email
-        },
-        data : {
-          is_verified  : true,
-          verify_otp : ""
-        }
-      });
-      return
-    } catch(error : any){
-      throw new CustomError(error.message)
     }
   }
-  public async updateUserResetOtp (user : User,resetOtpExpireAt : Date,resetOtp : string) : Promise<void>{
-      try {
-        await prisma.user.update({
-          where : {
-            email : user.email
-          },
-          data : {
-            reset_otp : resetOtp,
-            reset_otp_expire_at : resetOtpExpireAt
-          }
-        })
-      } catch(error : any){
-        throw new CustomError(error.message)
-      }
-  }
-    public async resetPassword(email : string,password : string,hashedPw : string) : Promise<void>{
+  public async updateUserVerified(user: User): Promise<void> {
+    const { email, is_verified, verify_otp } = user;
     try {
       await prisma.user.update({
-        where : {
-          email
+        where: {
+          email,
         },
-        data : {
-          password : hashedPw
-        }
-      })
-    } catch(error :any){
-      throw new CustomError(error.message)
+        data: {
+          is_verified: true,
+          verify_otp: "",
+        },
+      });
+      return;
+    } catch (error: any) {
+      throw new CustomError(error.message);
+    }
+  }
+  public async updateUserResetOtp(
+    user: User,
+    resetOtpExpireAt: Date,
+    resetOtp: string
+  ): Promise<void> {
+    try {
+      await prisma.user.update({
+        where: {
+          email: user.email,
+        },
+        data: {
+          reset_otp: resetOtp,
+          reset_otp_expire_at: resetOtpExpireAt,
+        },
+      });
+    } catch (error: any) {
+      throw new CustomError(error.message);
+    }
+  }
+  public async resetPassword(
+    email: string,
+    password: string,
+    hashedPw: string
+  ): Promise<void> {
+    try {
+      await prisma.user.update({
+        where: {
+          email,
+        },
+        data: {
+          password: hashedPw,
+        },
+      });
+    } catch (error: any) {
+      throw new CustomError(error.message);
     }
   }
 }
